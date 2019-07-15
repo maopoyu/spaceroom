1bcc12797116c03374750e9162387757449b077e
System Administrator <root@MaoBook-no-MacBook.local>
2019-07-15 23:12:31 +0900

    Build a drop down list

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
 	validates :name, presence: true
-	validates :room_num, presence: true, length: { minimum: 4, maximum: 4 }
+	validates :room_num, presence: true
 	validates :check_in, presence: true
 	validates :check_out, presence: true
+	validates :check_in, :check_out, overlap: { exclude_edges: :check_out, scope: 'room_num', message_content: '| The bed is occupied.'}
+
 end
