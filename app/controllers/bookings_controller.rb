92c0dfaf3fcd9de468e9405739a55bcfa4800106
System Administrator <root@MaoBook-no-MacBook.local>
2019-07-19 07:23:02 +0900

    Table: Try to combine cells, not finished

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,58 +1,54 @@
 class BookingsController < ApplicationController
-	before_action :set_booking, only: [:edit, :update, :show, :destroy]
+  before_action :set_booking, only: [:edit, :update, :show, :destroy]
 
-	def home
-	end
+  def home
+  end
 
-	def index
-		@bookings = Booking.all
-	end
+  def index
+  	@bookings = Booking.all
+  end
 
-	def edit
-		
-	end
+  def edit
+  end
 
-	def new
-		@booking = Booking.new
-	end
+  def new
+  	@booking = Booking.new
+  end
 
-	def create
-		@booking = Booking.new(booking_params)
-		if @booking.save
-			flash[:notice] = "Assignment was completed."
-			redirect_to booking_path(@booking)
-		else
-			render 'new'
-		end
-	end
+  def create
+  	@booking = Booking.new(booking_params)
+  	if @booking.save
+  	  flash[:notice] = "Assignment was completed."
+  	  redirect_to booking_path(@booking)
+  	else
+  	  render 'new'
+  	end
+  end
 
-	def update
-		
-		if @booking.update(booking_params)
-			flash[:notice] = "Assignment was edited."
-			redirect_to booking_path(@booking)
-		else
-			render 'edit'
-		end
-	end
+  def update		
+  	if @booking.update(booking_params)
+  	  flash[:notice] = "Assignment was edited."
+  	  redirect_to booking_path(@booking)
+  	else
+  	  render 'edit'
+  	end
+  end
 
-	def show
-		
-	end
+  def show
+  end
 
-	def destroy
-		
-		@booking.destroy
-		flash[:notice] = "Assignment is deleted."
-		redirect_to bookings_path
-	end
+  def destroy
+  	@booking.destroy
+  	flash[:notice] = "Assignment is deleted."
+  	redirect_to bookings_path
+  end
 
-	private
-	def set_booking
-		@booking = Booking.find(params[:id])
-	end
+  private
+  def set_booking
+  	@booking = Booking.find(params[:id])
+  end
 
-	def booking_params
-		params.require(:booking).permit(:name, :room_num, :check_in, :check_out)
-	end
+  def booking_params
+  	params.require(:booking).permit(:name, :room_num, :check_in, :check_out)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
-	validates :name, presence: true
-	validates :room_num, presence: true
-	validates :check_in, presence: true
-	validates :check_out, presence: true
-	validates :check_in, :check_out, overlap: { exclude_edges: :check_out, scope: 'room_num', message_content: '| The bed is occupied.'}
-
+  validates :name, presence: true
+  validates :room_num, presence: true
+  validates :check_in, presence: true
+  validates :check_out, presence: true
 end
